baec92387693ee83f6a43282b4f7d72ac43c37e7
Daniel Solin <daniel@solin.org>
2025-09-02 05:38:55 +0200

    feat: Migrate from Next.js to Eleventy

---
+++ .eleventy.js
@@ -0,0 +1,14 @@
+module.exports = function(eleventyConfig) {
+  return {
+    dir: {
+      input: ".",
+      includes: "_includes",
+      data: "_data",
+      output: "_site",
+    },
+    templateFormats: ["md", "njk", "html"],
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+  };
+};
---
+++ index.md
@@ -0,0 +1,8 @@
+---
+layout: layout.njk
+title: Hello World
+---
+
+# Hello, World!
+
+This is my first post on my new Eleventy blog.
